ed9369438f732466c6424a4cd0d76d1952c603d9
André Philipp Meuche <andre.philipp03@gmail.com>
2021-11-07 01:05:11 +0100

    added test

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,11 +1,31 @@
 import unittest
 
-from smolcalc import nodes, interpreter, values
+from smolcalc.nodes import *
+from smolcalc.interpreter import Interpreter
+from smolcalc.values import Number
 
 
 class TestInterpreter(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def test_numbers(self):
+        value = Interpreter(False).visit(number_node(123.456))
+        self.assertEqual(value, Number(123.456))
+
+    def test_single_operations(self):
+        result = Interpreter(False).visit(add_node(number_node(10), number_node(4.5)))
+        self.assertEqual(result.value, 14.5)
+
+        result = Interpreter(False).visit(subtract_node(number_node(10), number_node(4.5)))
+        self.assertEqual(result.value, 5.5)
+
+        result = Interpreter(False).visit(multiply_node(number_node(10), number_node(4.5)))
+        self.assertEqual(result.value, 45)
+
+        result = Interpreter(False).visit(divide_node(number_node(10), number_node(4.5)))
+        self.assertAlmostEqual(result.value, 2.22222,5)
+
+        with self.assertRaises(Exception):
+            Interpreter(False).visit(divide_node(number_node(10), number_node(0)))
 
 
 if __name__ == '__main__':
