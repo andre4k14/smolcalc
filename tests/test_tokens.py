c85c447141b152f62d7d6f2fb83d74ce454d43b8
André Philipp Meuche <andre.philipp03@gmail.com>
2023-06-23 18:15:02 +0200

    added test to make sure that every enum is in dict

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -1,6 +1,6 @@
 import unittest
 
-from smolcalc.tokens import Token, TokenType
+from smolcalc.tokens import Token, TokenType, tokens_to_string
 
 
 class TestToken(unittest.TestCase):
@@ -9,6 +9,10 @@ class TestToken(unittest.TestCase):
         self.assertEqual(repr(token), "NUMBER:-85.7")
         token = Token(TokenType.PLUS)
         self.assertEqual(repr(token), "PLUS")
+
+    def test_if_in_tokens_to_string_dict(self):  # tests if all TokenType enums are in the dict
+        for tt in TokenType:
+            self.assertEqual(True, tt in tokens_to_string)
 
 
 if __name__ == '__main__':
