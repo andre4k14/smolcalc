d259e61f0cc59d52ede7264f5d492a2039734249
André Philipp Meuche <andre.philipp03@gmail.com>
2023-06-23 17:45:24 +0200

    added dict with string representation of TokenType enums

--- src/smolcalc/tokens.py
+++ src/smolcalc/tokens.py
@@ -1,22 +1,38 @@
 from typing import Optional
 
-from enum import Enum
+from enum import Enum, auto
 from dataclasses import dataclass
 
 
 class TokenType(Enum):
-    NUMBER = 0
-    PLUS = 1
-    MINUS = 2
-    MULTIPLY = 3
-    DIVIDE = 4
-    L_BRACKET = 5
-    R_BRACKET = 6
-    FACTORIAL = 7
-    EXPONENT = 8
-    NLOG = 9
-    LOG_10 = 10
-    SQUARE_ROOT = 11
+    NUMBER = auto()
+    PLUS = auto()
+    MINUS = auto()
+    MULTIPLY = auto()
+    DIVIDE = auto()
+    L_BRACKET = auto()
+    R_BRACKET = auto()
+    FACTORIAL = auto()
+    EXPONENT = auto()
+    NLOG = auto()
+    LOG_10 = auto()
+    SQUARE_ROOT = auto()
+
+
+tokens_to_string = {
+    TokenType.NUMBER: "number",
+    TokenType.PLUS: "+",
+    TokenType.MINUS: "-",
+    TokenType.MULTIPLY: "*",
+    TokenType.DIVIDE: "/",
+    TokenType.L_BRACKET: "(",
+    TokenType.R_BRACKET: ")",
+    TokenType.FACTORIAL: "!",
+    TokenType.EXPONENT: "^",
+    TokenType.NLOG: "ln(",
+    TokenType.LOG_10: "lg(",
+    TokenType.SQUARE_ROOT: "sqrt("
+}
 
 
 @dataclass
